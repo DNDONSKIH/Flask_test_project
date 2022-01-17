4f0ed902411452112be87e9b2050a683991fcc9c
dnd <dndonskih@yandex.ru>
2022-01-17 16:24:11 +0300

    step1

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return 'Hello, world!!!1'
+@app.route('/<a>/<b>')
+def index(a, b):
+    return f'{a}/{b}Hello, world!!!1'
 
 
 if __name__ == '__main__':
